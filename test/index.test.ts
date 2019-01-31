5a54940bdcc6bf3353c1a3a24df1d7cf87063a89
guoxing <guoxing@tujia.com>
2019-01-31 16:29:49 +0800

    test init

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,14 +1,24 @@
 import TJMobBridge from '../src'
-
+const tjMobBridge = new TJMobBridge()
 /**
  * Dummy test
  */
-describe('Dummy test', () => {
+describe('TJMobBridge test', async () => {
   it('works if true is truthy', () => {
     expect(true).toBeTruthy()
   })
 
-  it('DummyClass is instantiable', () => {
-    expect(new TJMobBridge()).toBeInstanceOf(TJMobBridge)
+  it('TJMobBridge.getInstance is TJMobBridge', () => {
+    expect(TJMobBridge.getInstance()).toBeInstanceOf(TJMobBridge)
+  })
+
+  it('TJMobBridge can be init', async () => {
+    window.wx = {}
+    Object.defineProperty(window.navigator, 'userAgent', {
+      value: 'micromessenger test',
+      configurable: true
+    })
+    await tjMobBridge.init()
+    expect(tjMobBridge.getEnv()).toBe(3)
   })
 })
