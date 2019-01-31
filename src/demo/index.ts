063bc22759e2a4a5789837df421e899d2ce58211
guoxing <guoxing@tujia.com>
2019-01-31 10:44:46 +0800

    init

---
+++ src/demo/index.ts
@@ -0,0 +1,11 @@
+import TJMobBridge from '..';
+
+console.log('See this in your browser console: Typescript Webpack Starter Launched');
+
+async function init() {
+  const tjMobBridge = TJMobBridge.getInstance();
+  await tjMobBridge.init();
+  tjMobBridge.login();
+}
+
+init();
