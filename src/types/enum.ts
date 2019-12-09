ede2b5a80da520b80cc4abc30da3c3da9a78aa1b
guoxing <guoxing@tujia.com>
2019-12-09 18:52:18 +0800

    优化 enum 示例

--- src/types/enum.ts
+++ src/types/enum.ts
@@ -4,13 +4,5 @@ export enum PlatformType {
   // 浏览器环境
   BROWSER_ENV = 1,
   // app环境
-  APP_ENV = 2,
-  // 微信环境
-  WX_ENV = 3,
-  // 微信小程序环境
-  WX_MP_ENV = 4,
-  // 支付宝浏览器环境
-  ALI_ENV = 5,
-  // 支付宝小程序环境
-  ALI_MP_ENV = 6
+  APP_ENV = 2
 }
