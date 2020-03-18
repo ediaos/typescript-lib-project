2309f38dbf8d710cd93fbe0376ccb8583fed22e6
guoxing <guoxing@tujia.com>
2020-03-18 20:06:27 +0800

    优化&迁移 demo 文件

---
+++ demo/index.ts
@@ -0,0 +1,11 @@
+import testAddNumber from '..'
+
+console.log(
+  'See this in your browser console: Typescript Webpack Starter Launched'
+)
+
+function init() {
+  console.log('testAddNumber+10=', testAddNumber(10))
+}
+
+init()
